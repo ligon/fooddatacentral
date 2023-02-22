beee6ab40553499454b35854d35561fb8685f9bd
Ethan Ligon <ligon@berkeley.edu>
2023-02-21 18:40:48 -0800

    Move docstring to __init__.

--- fooddatacentral/__init__.py
+++ fooddatacentral/__init__.py
@@ -1,1 +1,21 @@
+"""Package to obtain information from the USDA's Food Data Central data project.
+
+See https://fdc.nal.usda.gov/api-guide.html for API documentation; see
+https://fdc.nal.usda.gov/api-key-signup.html for API key; please also refer to
+the documentation provided in the README.md.
+
+Optional txt file of units '.unitsrc' can live in the HOME directory with
+specification of necessary unit conversions (e.g., "extra_large_egg = 56 * grams
+= xl_egg" and "tea_bag = 3 * grams"). See documentation for the pint package at
+https://pint.readthedocs.io/.
+
+Functions:
+
+    search(str, str/int, url) -> pd.DataFrame
+    nutrients(str, int, url) -> pd.DataFrame
+    units(int/float, str , ureg) -> ureg
+    ingredients(str, int, url) -> pd.DataFrame
+
+"""
+
 from .fooddatacentral import *
--- fooddatacentral/fooddatacentral.py
+++ fooddatacentral/fooddatacentral.py
@@ -1,18 +1,3 @@
-"""
-Python module to obtain information from the USDA's Food Data Central data project.
-
-See https://fdc.nal.usda.gov/api-guide.html for API documentation; see https://fdc.nal.usda.gov/api-key-signup.html for API key; please also refer to the documentation provided in the README.md.
-
-Optional txt file of units '.unitsrc' can live in the HOME directory with specification of necessary unit conversions (e.g., "extra_large_egg = 56 * grams = xl_egg" and "tea_bag = 3 * grams").
-
-Functions:
-
-    search(str, str/int, url) -> pd.DataFrame
-    nutrients(str, int, url) -> pd.DataFrame
-    units(int/float, str , ureg) -> ureg
-    ingredients(str, int, url) -> pd.DataFrame
-"""
-
 from urllib.request import Request, urlopen
 import pandas as pd
 import json
